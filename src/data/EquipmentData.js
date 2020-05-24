0e3dcc71fc9e8e9e034a269bc3fd6692630a7cd9
Jojo Zhang <jojozhang0327@gmail.com>
2020-05-24 16:08:38 -0400

    Set up data files for media, library and equipment

---
+++ src/data/EquipmentData.js
@@ -0,0 +1,24 @@
+const EquipmentData = [
+  {
+    id: 1,
+    name: "Treadmills",
+    img: "../assets/images/Equipments/treadmillperson.png",
+  },
+  {
+    id: 2,
+    name: "Bikes",
+    img: "../assets/images/Equipments/bikeperson.png",
+  },
+  {
+    id: 3,
+    name: "Ellipticals",
+    img: "../assets/images/Equipments/ellipticalperson.png",
+  },
+  {
+    id: 1,
+    name: "Strength",
+    img: "../assets/images/Equipments/strengthperson.png",
+  },
+];
+
+export default EquipmentData;
---
+++ src/data/MediaData.js
@@ -0,0 +1,36 @@
+const MediaData = [
+  {
+    id: 1,
+    name: "Gear Junkie",
+    logo: "../assets/images/gearjunkie-vector-logo.png",
+    review:
+      "You focus on putting in the work, and the technology handles the rest.",
+  },
+  {
+    id: 2,
+    name: "Wired",
+    logo: "../assets/images/wired_logo.png",
+    review: "Literally transports you from home to wherever you choose to run.",
+  },
+  {
+    id: 3,
+    name: "Mashable",
+    logo: "../assets/images/mashable_logo.png",
+    review: "Breathes new life into a tired, old running routine.",
+  },
+  {
+    id: 4,
+    name: "Gear Junkie",
+    logo: "../assets/images/gearjunkie-vector-logo.png",
+    review:
+      "You focus on putting in the work, and the technology handles the rest.",
+  },
+  {
+    id: 5,
+    name: "Wired",
+    logo: "../assets/images/wired_logo.png",
+    review: "Literally transports you from home to wherever you choose to run.",
+  },
+];
+
+export default MediaData;

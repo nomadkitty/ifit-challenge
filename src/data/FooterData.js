8e288c1e38405f9f6fa6399f02781a97ee947d03
Jojo Zhang <jojozhang0327@gmail.com>
2020-05-24 16:38:03 -0400

    Set up component folders and files, add FooterData

---
+++ src/data/FooterData.js
@@ -0,0 +1,16 @@
+const FooterData = {
+  company: ["About", "Contact Us", "Careers"],
+  account: ["Log In", "Create Account"],
+  support: ["Help Center", "Accessibility"],
+  socialMedia: [
+    "../assets/icons/socialMedia/youtube.png",
+    "../assets/icons/socialMedia/pinterest.png",
+    "../assets/icons/socialMedia/facebook.png",
+    "../assets/icons/socialMedia/twitter.png",
+    "../assets/icons/socialMedia/instagram.png",
+  ],
+  language: ["English"],
+  legal: ["© iFit.com. All Rights Reserved.", "Privacy Policy", "Terms of Use"],
+};
+
+export default FooterData;

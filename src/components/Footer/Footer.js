fad0681f82129a02c3fdada57db1a3321f5ace16
Jojo Zhang <jojozhang0327@gmail.com>
2020-05-25 22:08:50 -0400

    finish footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -40,7 +40,26 @@ const Footer = () => {
           </ul>
         </div>
       </div>
-      <div className="socialMedia"></div>
+      <div className="socialMedia">
+        {FooterData.socialMedia.map((ele) => (
+          <img src={ele} alt="social media" />
+        ))}
+      </div>
+      <div className="bottom">
+        <div className="language">
+          <select name="language" id="language">
+            <option value="english">English</option>
+            <option value="chinese">Chinese</option>
+            <option value="spanish">Spanish</option>
+            <option value="german">German</option>
+          </select>
+        </div>
+        <div className="legal">
+          <p>© iFit.com. All Rights Reserved.</p>
+          <Link to="#">Privacy Policy</Link>
+          <Link to="#">Terms of Use</Link>
+        </div>
+      </div>
     </div>
   );
 };
